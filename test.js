614caa4c4a3857d35e0452e0939b24fd1d7bd521
Hasnain <muhammadhassain55@gmail.com>
2024-01-08 20:42:11 +0500

    modify

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
-console.log("Hello");
+//console.log("Hello");
 
-console.log("Hello hasnain");
+//console.log("Hello hasnain");
+
+var a =5;
+var b=4;
+var c="hasnain";
+
+console.log(a+b+12);
+console.log(c);
+
+console.log(typeof a,typeof b,typeof c)
+
