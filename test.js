469acdcbbcd77b0e106f0a661ace0d9a17ae3023
muhammad Ali Anjum <anjumbalgharii@gmail>
2024-01-09 15:13:01 +0500

    varibal

--- test.js
+++ test.js
@@ -1,2 +1,21 @@
 console.log("Hello");
 console.log("Hi this is anjum");
+
+
+// console.log(a); ReferenceError: Cannot access 'a' before initialization
+let a=40;
+
+
+// let  a=50;    SyntaxError: Identifier 'a' has already been declared  
+console.log(a);
+const b=20;
+// const b=40;SyntaxError: Identifier 'b' has already been declared
+console.log(b);
+var v=30;
+var v=40;
+console.log(v);
+// let z=kdnnf; ReferenceError: kdnnf is not defined
+let z='Anjum';
+console.log(z);
+const d="anjum";
+console.log(d);
