161fd1b4d288832c01f0eeee76283c311401894d
Jalal Hussain <jalal143880@gmail.com>
2024-01-09 15:06:18 +0500

    Modified test.js file.

--- test.js
+++ test.js
@@ -1,1 +1,29 @@
 console.log("Hello Jalal Hussain");
+const a = 2;
+const myName = "Jalal HUssain";
+const isMale = true;
+console.log(a);
+console.log("Name =", myName);
+console.log("isMale =", isMale);
+
+{
+  let a = 10;
+  // let a = 23;
+  console.log(a);
+}
+{
+  let a = 2;
+  let b = 67;
+  console.log(a);
+  console.log("inside block  b =", b);
+}
+{
+  // var a = 10;
+  // const a = 45;
+  var b = 34;
+  // let a = 23;
+  // console.log(a);
+  console.log("inside block  b =", b);
+}
+
+console.log("outside block  b =", b);
