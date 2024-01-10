e6c3503431de167365b2bfdae0e705c66fd4bb64
Hasnain <muhammadhassain55@gmail.com>
2024-01-10 12:08:33 +0500

    Apply object and array in js

---
+++ object.js
@@ -0,0 +1,14 @@
+//object in js
+const person = {
+    firstName:"John", 
+    lastName:"Doe"
+               };
+    person.age=43;
+console.log(person)
+
+//Array in js
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+
+const car = ["Saab", "Volvo", "BMW"];
+console.log(car[2])
