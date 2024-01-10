0cd4264dcfc14b7b2e4fecd418266d22fe91e48d
Hasnain <muhammadhassain55@gmail.com>
2024-01-10 12:31:57 +0500

    apply array

--- object.js
+++ object.js
@@ -1,14 +1,65 @@
 //object in js
-const person = {
-    firstName:"John", 
-    lastName:"Doe"
-               };
-    person.age=43;
-console.log(person)
+// const person = {
+//     firstName:"John", 
+//     lastName:"Doe"
+//                };
+//     person.age=43;
+// console.log(person)
 
-//Array in js
-const cars = ["Saab", "Volvo", "BMW"];
-console.log(cars);
+// //Array in js
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars);
 
-const car = ["Saab", "Volvo", "BMW"];
-console.log(car[2])
+// const car = ["Saab", "Volvo", "BMW"];
+// console.log(car[2])
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 50,
+//   eyeColor: "blue"
+// };
+
+// //console.log(person)
+
+// //console.log(person.eyeColor);
+
+// person.age=46;
+
+// console.log(person);
+
+// const person = {
+//   firstName: "John",
+//   lastName : "Doe",
+//   id       : 5566,
+
+// };
+
+// console.log(person)
+
+
+//   const car = [
+//   "Saab",
+//   "Volvo",
+//   "BMW"
+// ]
+
+// console.log(car)
+
+// const cars = [];
+// cars[0]= "Saab";
+// cars[1]= "Volvo";
+// cars[2]= "BMW";
+
+// console.log(cars[2])
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let length = fruits.length;
+
+// console.log(fruits.length)
+// console.log(fruits.length[3])
+
+// console.log(fruits[1].length)
+
+
