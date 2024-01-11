47a8fdfbf4dc6d0450f88f47ff5c174df643f0c1
Hasnain <muhammadhassain55@gmail.com>
2024-01-11 11:59:59 +0500

    Apply conditions

--- object.js
+++ object.js
@@ -54,13 +54,106 @@
 // console.log(cars[2])
 
 
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-let length = fruits.length;
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let length = fruits.length;
 
-console.log(fruits.length)
-console.log(fruits.length[3])
+// console.log(fruits.length)
+// console.log(fruits.length[3])
 
-console.log(fruits[1].length)
-console.warn(fruits);
+// console.log(fruits[1].length)
+// console.warn(fruits);
 
 
+// let x = 5;
+// let y = 2;
+// let z = x + y;
+// console.log(z)
+
+
+//JavaScript Operators
+
+// let x = 5;
+// let y = 2;
+// let z = x * y;
+// console.log(z)
+
+// let x = 5;
+// let y = 2;
+// let z = x * y;
+// console.log(z)
+
+// let x = 5;
+// let y = 2;
+// let z = x % y;
+// console.log(z)
+
+// let x = 5;
+// let y = 2;
+// let z = x / y;
+// console.log(z)
+
+//JavaScript Arithmetic Operators
+// let a = 3;
+// let x = (100 + 50) * a;
+
+// console.log("X:",x)
+
+// let a=12;
+// let b=2;
+// if(a!==b){
+//     console.log(true)
+// }else console.log(false)
+
+// const aliAge=28;
+// const akbarAge=25;
+// if(aliAge>=akbarAge){
+//     console.log(true)
+// }else console.log(false)
+
+
+// const a = 2, b = 'hello';
+// if(a !== 2){console.log(true)}else console.log(false)
+// console.log(a !== '2') 
+// console.log(b !== 'Hello'); 
+
+// check if the number is positive
+
+// const number = -2354;
+
+// // check if number is greater than 0
+// if (number > 0) {
+//  // the body of the if statement
+//   console.log(" number is greater than 0");
+// }else
+
+// console.log("number is not greater than 0");
+
+// let num = 21; 
+  
+// if (num % 2 === 0) { 
+//     console.log("Given number is even number."); 
+// } if(num % 2 !== 0) { 
+//     console.log("Given number is odd number."); 
+// };
+
+
+// let age = 12; 
+  
+// if (age >= 18) { 
+//     console.log("You are eligible of driving licence") 
+// } else { 
+//     console.log("You are not eligible for driving licence") 
+// };
+
+
+
+
+const num = -19; 
+  
+if (num > 0) { 
+    console.log("Given number is positive."); 
+} else if (num < 0) { 
+    console.log("Given number is negative."); 
+} else { 
+    console.log("Given number is zero."); 
+};
