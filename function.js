68d9f061cd02ae665502806b729b04f5d7593b3b
Jalal Hussain <jalal143880@gmail.com>
2024-01-15 22:17:05 +0500

    Modified function.js file.

--- function.js
+++ function.js
@@ -49,6 +49,7 @@
 // case3(param);
 // case4();
 
+//------------------------------------------------
 let message = (data) => {
   // console.log("From Message Function");
   console.log(`${data}`);
@@ -76,9 +77,9 @@ console.log(mycar.make); // "Toyota"
 //-------------------------------------------------
 // program to find the square of a number
 // function is declared inside the variable
-let x = function (num) { return num * num };
-console.log(x(4));
+let sqr = function (num) { return num * num };
+console.log(sqr(4));
 
 // can be used as variable value for other variables
-let y = x(3);
+let y = sqr(3);
 console.log(y);
