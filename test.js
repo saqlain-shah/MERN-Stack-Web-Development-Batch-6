c4ec7dc3cccb0ff4fca0a29d661c89dc9af72205
Jalal Hussain <jalal143880@gmail.com>
2024-01-15 22:11:58 +0500

    Modified test.js file.

--- test.js
+++ test.js
@@ -172,53 +172,53 @@ console.log("Hello Jalal Hussain");
 
 // FUNCTION
 
-function add(a, b) {
-  console.log("Sum of two numbers");
-  let sum = a + b;
-  console.log(`sum = ${sum}`);
-}
+// function add(a, b) {
+//   console.log("Sum of two numbers");
+//   let sum = a + b;
+//   console.log(`sum = ${sum}`);
+// }
 
 // let num1 = 2;
 // let num2 = 4;
 // add(num1, num2);
 //console.log("Hello Jalal Hussain");
-const a = 2;
-const myName = "Jalal HUssain";
-const isMale = true;
+// const a = 2;
+// const myName = "Jalal HUssain";
+// const isMale = true;
 //console.log(a);
 //console.log("Name =", myName);
 //console.log("isMale =", isMale);
 
-{
-  let a = 10;
-  // let a = 23;
-  // console.log(a);
-}
-{
-  let a = 2;
-  let b = 67;
+// {
+//   // let a = 10;
+//   // let a = 23;
+//   // console.log(a);
+// }
+// {
+//   let a = 2;
+//   let b = 67;
   // console.log(a);
   //console.log("inside block  b =", b);
-}
-{
+// }
+// {
   // var a = 10;
   // const a = 45;
   var b = 34;
   // let a = 23;
   // console.log(a);
   // console.log("inside block  b =", b);
-}
+// }
 
 //console.log("outside block  b =", b);
 
-// Arrow function
-let mult = (a, b) => {
-  console.log("product of two numbers");
-  let prod = a * b;
-  console.log(`product = ${prod}`);
-};
+// // Arrow function
+// let mult = (a, b) => {
+//   console.log("product of two numbers");
+//   let prod = a * b;
+//   console.log(`product = ${prod}`);
+// };
 
-mult(2, 3);
+// mult(2, 3);
 //console.log(student);
 
 // let person2 = {
@@ -230,21 +230,49 @@ mult(2, 3);
 // console.log(person2);
 
 // array
-let marks = [23, 34, 46, 56];
+// let marks = [23, 34, 46, 56];
 
 //console.log(marks);
 
-let num1 = 1;
-let num2 = 2;
-//let sum = ++num1 + --num2;
-//console.log(sum);+
-if (num1 !== num2) {
-  console.log("True");
-} else console.log("False");
+// let num1 = 1;
+// let num2 = 2;
+// //let sum = ++num1 + --num2;
+// //console.log(sum);+
+// if (num1 !== num2) {
+//   console.log("True");
+// } else console.log("False");
 
-if (num1 > num2 && num2 + 1 == num2) {
-  console.log("True");
-} else console.log("False");
-if (num1 + num2 || num1++ > num2) {
-  console.log("True");
-} else console.log("False");
+// if (num1 > num2 && num2 + 1 == num2) {
+//   console.log("True");
+// } else console.log("False");
+// if (num1 + num2 || num1++ > num2) {
+//   console.log("True");
+// } else console.log("False");
+
+// Loops in JavaScript
+
+let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+let myName = prompt("Enter your name:");
+const countvowels = (myName) => {
+  let count = 0;
+  for (const char of myName) {
+    if (
+      char === vowels[0] ||
+      char === vowels[1] ||
+      char === vowels[2] ||
+      char === vowels[3] ||
+      char === vowels[4] ||
+      char === vowels[5] ||
+      char === vowels[6] ||
+      char === vowels[7] ||
+      char === vowels[8] ||
+      char === vowels[9] ||
+      char === vowels[10]
+    ) {
+      count++;
+    }
+  }
+  return(count);
+};
+
+console.log("Number of vowels in '" + myName + "' is " + countvowels(myName) + ".");
