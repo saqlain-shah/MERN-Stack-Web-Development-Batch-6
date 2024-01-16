fe8c1e26ecbb80bcf9ced740ed405e40a58ee7c6
Hasnain <muhammadhassain55@gmail.com>
2024-01-16 15:59:40 +0500

    Apply conditions in javascripts

--- conditions.js
+++ conditions.js
@@ -1,8 +1,20 @@
-let isood=true,
+let isodd=true,
 iseven=false;
 
 for(let i=0; i<=10; i++){
-    if(1%2==0){
+    if(i%2==0){
         console.log(`Even Value ${i}`);
+    }
+}
+
+for(let j=0; j<=10; j++){
+    if(j%2!=0){
+        console.log(`odd Value ${j}`);
+    }
+}
+
+for(let i=0; i<=30; i++){
+    if(i%4==0){
+        console.log(`multipal of 4 is  ${i}`);
     }
 }
