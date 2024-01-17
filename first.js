769a956b2e4da5d4cd8c653633105416ee805c88
sadaashraf <nayeemashraf92@gmail.com>
2024-01-17 17:57:03 +0500

    new files added

---
+++ first.js
@@ -0,0 +1,13 @@
+
+    let arr=["ali","akmal","sohal"];
+    console.log("Show the element in the Array",arr);
+    
+    let join=arr.join(",");
+    console.log("Array convart in String",join);
+    
+    
+    let sli=arr.slice(1,3);
+    console.log("Slicing int thr array",sli);
+
+
+
