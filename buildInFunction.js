d6224634db9f061e48856f554e4786172cec1fca
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-01-18 18:57:59 +0500

    add build In function

---
+++ buildInFunction.js
@@ -0,0 +1,18 @@
+
+var array = [1,'sjahdi',23.5,'c'];
+console.log(array)
+
+for(var i=0;i<array.length;i++){
+    console.log(array[i])
+}
+// pop,push,shift,unshift
+
+console.log(array.pop())
+console.log(array)
+console.log(array.push(34))
+
+console.log(array.shift())
+console.log(array.unshift('ali'))
+console.log(array)
+
+console.log(array.indexOf('ali'))
