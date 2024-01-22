1681f7337454972d736131a6a0363bb8099603c9
Jalal Hussain <jalal143880@gmail.com>
2024-01-22 14:47:46 +0500

    Modified destructuring.js file.

--- destructuring.js
+++ destructuring.js
@@ -1,10 +1,10 @@
-// Object Destructuring
+// Destructuring Object
 // assigning object attributes to variables
 const person = {
-    myName: 'Jalal',
-    age: 25,
-    gender: 'Male'    
-}
+  myName: "Jalal",
+  age: 25,
+  gender: "Male",
+};
 
 // destructuring assignment
 let { myName, age, gender } = person;
@@ -14,43 +14,44 @@ console.log(age); // 25
 console.log(gender); // Male
 
 //--------------------------------------------
-// Array Destructuring
+// Destructuring Array
 
-const arrValue = ['one', 'two', 'three'];
+const user = ["Jalal", 20, "Skardu"];
 // destructuring assignment in arrays
-const [x, y, z] = arrValue;
+const [name3, age3, coutry] = user;
 
-console.log(x); // one
-console.log(y); // two
-console.log(z); // three
+console.log(name3); // Jalal
+console.log(age3); // 20
+console.log(city); // Skardu
 
 // Assign Default Values
 
 let arrValue1 = [10];
 
 // assigning default value 5 and 7
-let [a = 5,  b = 7] = arrValue1;
+let [a = 5, b = 7] = arrValue1;
 
 console.log(a); // 10
 console.log(b); // 7
 
+// ----------------------------------
 const person1 = {
-    name1: 'Jalal',
-}
+  name1: "Jalal",
+};
 
 // assign default value 26 to age if undefined
-const { name1, age1 = 26} = person;
+const { name1, age1 = 26 } = person;
 
 console.log(name1); // Jalal
 console.log(age1); // 2
 
 // Assign Remaining Elements to a Single Variable
 // You can assign the remaining elements of an array to a variable using the 'spread' syntax "...".
-const arrValue2 = ['one', 'two', 'three', 'four'];
+const arrValue2 = ["one", "two", "three", "four"];
 
 // destructuring assignment in arrays
 // assigning remaining elements to y
-const [c, ...d] = arrValue;
+const [c, ...d] = arrValue2;
 
 console.log(c); // one
 console.log(d); // ["two", "three", "four"]
@@ -58,10 +59,10 @@ console.log(d); // ["two", "three", "four"]
 // You can also assign the rest of the object properties to a single variable
 
 const person2 = {
-    name2: 'Jalal',
-    age2: 25,
-    gender2: 'Male'    
-}
+  name2: "Jalal",
+  age2: 25,
+  gender2: "Male",
+};
 
 // destructuring assignment
 // assigning remaining properties to rest
@@ -69,3 +70,5 @@ let { name2, ...rest } = person;
 
 console.log(name2); // Jalal
 console.log(rest); // {age2: 25, gender2: "Male"}
+
+// ---------------------------------------------------
