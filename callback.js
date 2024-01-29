fc66fc1911f09908a21d11ab64712a3f1df9012c
Hasnain <muhammadhassain55@gmail.com>
2024-01-29 15:12:13 +0500

    modifi

--- callback.js
+++ callback.js
@@ -28,19 +28,16 @@
 // sayName('John');
 
 
-// Callback Function Example
-function greet(name, myFunction) {
-    console.log('Hello world');
+// // Callback Function Example
+// function greet(name, myFunction) {
+//     console.log('Hello world');
 
-    // callback function
-    // executed only after the greet() is executed
-    myFunction(name);
-}
+//     // callback function
+//     // executed only after the greet() is executed
+//     myFunction(name);
+// }
 
-// callback function
-function sayName(name) {
-    console.log('Hello' + ' ' + name);
-}
-
-// calling the function after 2 seconds
-setTimeout(greet, 2000, 'John', sayName);
+// // callback function
+// function sayName(name) {
+//     console.log('Hello' + ' ' + name);
+// }
