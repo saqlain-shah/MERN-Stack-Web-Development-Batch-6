5a3820945d8e5fef7ac22a0b5cba3050d23393b9
Jalal Hussain <jalal143880@gmail.com>
2024-02-06 16:11:45 +0500

    Modified ReactJs folder.

--- React_Js/react-app/src/App.jsx
+++ React_Js/react-app/src/App.jsx
@@ -1,7 +1,8 @@
+import Home from "./Home";
 function App() {
-
   return (
     <>
+      <Home />
       <h1>Jalal Hussain</h1>
       <p>I'm Jalal Hussain, MERN stack intern at Rinor.</p>
     </>
