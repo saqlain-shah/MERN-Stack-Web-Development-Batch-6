929edb6c61d68eea19d6e05c6dddc90ecff6b0ad
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-02-07 15:03:48 +0500

    add new file

--- react/ReactProject/src/App.jsx
+++ react/ReactProject/src/App.jsx
@@ -1,14 +1,14 @@
-
-
+import FirstComponent from "./component/firsComponent";
 
 function App() {
 
   return (
     <>
      <h1>Anjum </h1>
-     <p>this is my first react project</p>
+     <p>hello there </p>
+     <FirstComponent/>
     </>
-  )
+  );
 }
 
 export default App
