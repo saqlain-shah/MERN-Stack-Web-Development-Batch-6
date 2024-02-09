133b3d9e707cee7149920bd09e546858867cb716
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-02-09 17:17:42 +0500

    add new file react

--- react/ReactProject/src/App.jsx
+++ react/ReactProject/src/App.jsx
@@ -1,14 +1,19 @@
 import FirstComponent from "./component/firsComponent";
+import Counter from "./component/Counter";
+import Login from "./component/login";
 
 function App() {
 
   return (
     <>
-     <h1>Anjum </h1>
+     {/* <h1>Anjum </h1>
      <p>hello there </p>
      <FirstComponent/>
+     <Counter/> */}
+     <Login/>
     </>
   );
 }
 
 export default App
+
---
+++ react/ReactProject/src/component/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleUsernameChange = (event) => {
+      setUsername(event.target.value);
+    };
+  
+    const handlePasswordChange = (event) => {
+      setPassword(event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      // Here you can perform validation and authentication logic
+      console.log('Username:', username);
+      console.log('Password:', password);
+      // Reset form fields after submission
+      setUsername('');
+      setPassword('');
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    );
+  };
+  
+  export default Login;
